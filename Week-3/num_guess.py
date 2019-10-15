f254ce6e99a586eaa62d634992547179fca980ec
IrinaAlBa <56313129+IrinaAlBa@users.noreply.github.com>
2019-10-15 22:53:04 +0100

    adding loops

--- Week-3/num_guess.py
+++ Week-3/num_guess.py
@@ -3,4 +3,21 @@ guess = input("Take a wild guess: ")
 guess = int(guess)
 number = random.randint(1,10)
 if guess == number:
-    print(guess)
+    print("You are right!")
+else:
+    print("You are wrong.")
+    num_tries = 1
+    while num_tries < 3:
+        print('You have ' + str(3-num_tries) + ' tries left. Try again.')
+        num_tries = num_tries + 1
+        print('Take a wild guess: ')
+        guess = input()
+        guess = int(guess)
+        number = random.randint(1,10)
+        if guess == number
+            print("You are right!")
+        else:
+            continue
+     if num_tries == 3:
+        print('You lose.')
+    break
