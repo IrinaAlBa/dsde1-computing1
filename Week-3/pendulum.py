9df29cf2f95202d301ac2cef33dcb893468e1af5
IrinaAlBa <56313129+IrinaAlBa@users.noreply.github.com>
2019-10-19 10:04:48 +0100

    edited pendulum

--- Week-3/pendulum.py
+++ Week-3/pendulum.py
@@ -1,14 +1,15 @@
 import math
 def period(L,g):
-    x = isinstance(L, int)
-    if x == False:
-        raise Exception("There is something wrong here...")
-    y = isinstance(g, int)
-    if y == False:
-        raise Exception("There is something wrong here...")
-    print(x)
-    print(y)
+    x = isinstance(L, (int,float))
+    if not x:
+        raise TypeError("L must be a number")
+    y = isinstance(g, (int,float))
+    if not y:
+        raise TypeError("g must be a number")
+    if L<= 0:
+        raise ValueError("L must be positive")
+    y = isinstance(g, (int,float))
+    if g<= 0:
+        raise ValueError("g must be positive")
     T = 2*math.pi*((L/g)**(1/2))
-    print(T)
-
-period(10,5)
+    print(f'{T:.2f}')
