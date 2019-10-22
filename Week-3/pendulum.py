698047bb029f3641900c4f25df21435e835a55da
IrinaAlBa <56313129+IrinaAlBa@users.noreply.github.com>
2019-10-22 11:58:04 +0100

    added previously deleted files

---
+++ Week-3/pendulum.py
@@ -0,0 +1,16 @@
+
+import math
+def period(L,g):
+    x = isinstance(L, (int,float))
+    if not x:
+        raise TypeError("L must be a number")
+    y = isinstance(g, (int,float))
+    if not y:
+        raise TypeError("g must be a number")
+    if L<= 0:
+        raise ValueError("L must be positive")
+    y = isinstance(g, (int,float))
+    if g<= 0:
+        raise ValueError("g must be positive")
+    T = 2*math.pi*((L/g)**(1/2))
+    print(f'{T:.2f}')
