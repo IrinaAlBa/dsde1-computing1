f539a022a473660d2ce8c448f380981071bf002c
IrinaAlBa <56313129+IrinaAlBa@users.noreply.github.com>
2019-10-23 17:47:05 +0100

    linted fuzzy.py until 10/10 score

--- Week-4/fuzzy.py
+++ Week-4/fuzzy.py
@@ -6,27 +6,25 @@ Lint this file using PyLint.
 
 # This function does some maths on three numbers.
 def maths(input_a, input_b, input_c):
+    """ This function does some maths on three numbers"""
     result = input_a * 3 - input_b + input_c
     return result
 
 # This function returns True or False.
 def choices(question):
-    if(question == True):
-        return True
-    else:
-        return False
+    """This function returns True or False."""
+    return bool(question)
 
-
-def main():
-    this_variable = 'useless'
-    
     # first function takes three numbers
-    answer = maths(3, 9 , 2.3)
-    print(answer)
-
     # second function takes a True or False
-    newAnswer = choices(True)
-    print(newAnswer)
+def main():
+    """The first function takes three numbers.
+    The second function takes a True or False."""
+    answer = maths(3, 9, 2.3)
+    print(answer)
+    new_answer = choices(True)
+    print(new_answer)
 
 if __name__ == '__main__':
     main()
+    
