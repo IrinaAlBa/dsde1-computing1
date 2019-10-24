9b2946c58cee89c469d2a890c5d89503a9133094
IrinaAlBa <56313129+IrinaAlBa@users.noreply.github.com>
2019-10-24 14:47:12 +0100

    wrote welcome message

--- Week-4/keywords.py
+++ Week-4/keywords.py
@@ -13,6 +13,20 @@ with default values.
 # it returns 'Hello and welcome to <place>'
 # if both user_name and place are provided
 # it returns 'Hello, <user_name>, and welcome to <place>
+def welcome_message(user_name = 0, place = 0):
+    if user_name == 0 and place == 0:
+        print("Hello and welcome")
+    if user_name !=0 and place == 0:
+        print("Hello " + str(user_name) + " and welcome")
+    if user_name != 0 and place == 0:
+        print("Hello " + str(user_name) + " and welcome to " + str(place))
+
+
+def list_average(a_list, avg_type):
+    a_list = []
+
+    if avg_type == "mean":
+        return
 
 
 # Create a function called list_average()
@@ -20,7 +34,7 @@ with default values.
 # (the point of this exercise is to practice interacting 
 # with lists)
 # the first argument is a list of numbers
-# the second optional keyword arguemt is called avg_type
+# the second optional keyword argument is called avg_type
 # if nothing for avg_type provided, return the mean of the list
 # if avg_type='mode', return the mode of the list 
 # (return list of all modes if there is a tie between multiple values)
