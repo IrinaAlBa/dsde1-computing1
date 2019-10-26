af69514856c03a6a6bff0bbba1be28e859221622
IrinaAlBa <56313129+IrinaAlBa@users.noreply.github.com>
2019-10-26 09:50:32 +0100

    mean, mode and median without libs

--- Week-4/keywords.py
+++ Week-4/keywords.py
@@ -22,12 +22,30 @@ def welcome_message(user_name = 0, place = 0):
         print("Hello " + str(user_name) + " and welcome to " + str(place))
 
 
-def list_average(a_list, avg_type):
-    a_list = []
-
-    if avg_type == "mean":
-        return
-
+def list_average(a_list, avg_type = 0):
+    n = len(a_list)
+    get_sum = sum(a_list)
+    mean = get_sum / n
+    a_list.sort()
+    if n % 2 == 0: 
+        median1 = a_list[n//2] 
+        median2 = a_list[n//2 - 1] 
+        median = (median1 + median2)/2
+    else:
+        median = a_list[n//2]
+    from collections import Counter
+    data = Counter(a_list)
+    get_mode = dict(data) 
+    mode = [k for k, v in get_mode.items() if v == max(list(data.values()))]
+    if avg_type == 0 or avg_type == "mean":
+        return mean
+    if avg_type == "mode":
+        if len(mode) == n: 
+            get_mode = "No mode found"
+        else: 
+            get_mode = "Mode is / are: " + ', '.join(map(str, mode)) 
+    if avg_type == "median":
+        return median
 
 # Create a function called list_average()
 # without using any libraries to do the maths for you 
